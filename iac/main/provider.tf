12955728ed731721800b13a68d8560cd3db5c9d3
tornike <tornike@bcw.group>
2024-08-23 17:27:53 +0400

    refactoring code and editing readme

--- iac/main/provider.tf
+++ iac/main/provider.tf
@@ -7,9 +7,38 @@ terraform {
     }
   }
   backend "gcs" {
-    bucket = <your bucket name>
+    bucket = <your bucket name> #Bucket name for 
     prefix = "solana-bq/bq"
   }
+}
+
+variable "project_id" { ###Name of a freshly created project
+  default = <your project id>
+}
+variable "region" {
+  description = "The zone for the instance"
+  default     = "us-central1"
+}
+variable "cird_range" {
+  default = "10.0.0.0/24"
+}
+
+variable "app_vm_size" {
+  description = "The VM size for the applications"
+  default     = "n2d-standard-16"
+}
+
+
+locals {
+  scopes = [
+    "https://www.googleapis.com/auth/devstorage.read_only",
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring.write",
+    "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/servicecontrol",
+    "https://www.googleapis.com/auth/trace.append",
+    "https://www.googleapis.com/auth/cloud-platform"
+  ]
 }
 
 provider "google" {
